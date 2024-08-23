f01b91afc4c9e0a67caad37c7bf7de6cedd12e5e
tiezi <2338699727@qq.com>
2024-08-23 17:38:18 +0800

    加载优化

---
+++ assets/throttle-p_HbOzza.js
@@ -0,0 +1,1 @@
+import{d as n}from"./debounce-CQIwcQi8.js";import{a8 as l}from"./index-g7kVX9AL.js";var g="Expected a function";function u(t,a,r){var i=!0,e=!0;if(typeof t!="function")throw new TypeError(g);return l(r)&&(i="leading"in r?!!r.leading:i,e="trailing"in r?!!r.trailing:e),n(t,a,{leading:i,maxWait:a,trailing:e})}export{u as t};
